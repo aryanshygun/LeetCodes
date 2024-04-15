128e52f42f332d907b7b3016ee4e2330624786f1
aryanshygun <aryanshygun5272@gmail.com>
2024-04-15 19:08:31 +0330

    commit

--- _ copy 9.py
+++ _ copy 9.py
@@ -1,18 +1,15 @@
-def makeGood(s: str) -> str:
-    if len(str) == 1:
-        return str
-    xlist = [*s]
-    l = 0
-    while l < len(xlist) - 1:
-        x = xlist[l]
-        y = xlist[l+1]
-        if (x.lower() == y or x == y.lower()) and x != y:
-            xlist.pop(l)
-            xlist.pop(l)
-            l = 0
-        else:
-            l +=1    
-    return ''.join(xlist)
-# s = "leEeetcode"
-s = "abBAcC"
-print(makeGood(s))
+
+def randomcolor():
+    import random
+    
+    rgba_seq = []
+    for i in range(10):
+        r_random = float("{:.5f}".format(random.random()))
+        g_random = float("{:.5f}".format(random.random()))
+        b_random = float("{:.5f}".format(random.random()))
+        rgba_seq.append([r_random, g_random, b_random, 1])
+    return random.choice(rgba_seq)
+
+
+print(randomcolor())
+print(randomcolor())
